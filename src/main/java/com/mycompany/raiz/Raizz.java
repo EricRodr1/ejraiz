e020959e086948ef01abd6cc8b38574fb639be09
Eric Rodriguez <Eric Rodriguez@ERIC-RODRIGUEZ.bbrouter>
2024-12-14 20:39:37 -0600

    Raiz

---
+++ src/main/java/com/mycompany/raiz/Raizz.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.raiz;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Eric Rodriguez
+ */
+public class Raizz {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        double raiz;
+        
+        System.out.println("Favor ingresar un numero");
+        raiz = Math.sqrt(entrada.nextDouble());
+        
+        System.out.println("La raiz cuadrada es; "+raiz);
+    }
+}
